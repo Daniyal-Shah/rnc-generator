ae0a897db1eb4c8cfb45055916665ba1d42cd298
Daniyal Shah <daniyal.shah.cs@gmail.com>
2025-05-14 23:05:38 +0500

    Updated naming convention for folder when created component.

--- bin/index.js
+++ bin/index.js
@@ -5,20 +5,37 @@ const path = require("path");
 
 const componentName = process.argv[2];
 
+const getType = (str) => {
+  if (str === "s") return "Screen";
+  else if (str === "c") return "Component";
+};
+
+if (process.argv[3] && process.argv[3] !== "s" && process.argv[3] !== "c") {
+  console.log(process.argv[3]);
+  console.log("❌ Either it can be c for Component and s for Screen ");
+  process.exit(1);
+}
+
 if (!componentName) {
   console.log("❌ Please provide a component name.");
   process.exit(1);
 }
 
-const componentDir = path.join(process.cwd(), componentName);
+const componentDir = path.join(
+  process.cwd(),
+  `${componentName}${getType(process.argv[3])}`
+);
 fs.mkdirSync(componentDir);
 
 fs.writeFileSync(
-  path.join(componentDir, `${componentName}.component.tsx`),
+  path.join(
+    componentDir,
+    `${componentName}.${getType(process.argv[3]).toLowerCase()}.tsx`
+  ),
   `import React from "react";
     import { View, Text } from "react-native";
 
-    const ${componentName}Component = () => {
+    const ${componentName}${getType(process.argv[3])} = () => {
         return (
             <View>
                 <Text>${componentName}</Text>
@@ -26,7 +43,7 @@ fs.writeFileSync(
         );
     };
 
-    export default ${componentName}Component;
+    export default ${componentName}${getType(process.argv[3])};
    `
 );
 
